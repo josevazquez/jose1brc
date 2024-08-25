7f194cc688aeb45ffe12af7511dbc3a869b5d76d
Jose Vazquez <jose@teguz.com>
2024-08-25 15:56:49 -0500

    Made parsing functions mutating, and cleaned up a bit

--- Sources/main.swift
+++ Sources/main.swift
@@ -26,25 +26,24 @@ public extension Subbuffer {
         }
         return self[start..<end]
     }
-
+    
     var string: String {
         return String(decoding: base[startIndex..<endIndex], as: UTF8.self)
     }
+    
+    mutating func parseUntilSemicolon() throws -> String {
+        let match = prefix { $0 != 0x3B } // hex ascii code for ;
+        self = dropFirst(match.count + 1)
+        return match.string
+    }
+    
+    mutating func parseUntilNewline() throws -> String {
+        // hex ascii code for carriage return or new line
+        let match = prefix { $0 != 0x0D && $0 != 0x0A }
+        self = dropFirst(match.count + 1)
+        return match.string
+    }
 }
-
-func parseUntilSemicolon(from input: inout Subbuffer) throws -> String {
-    let match = input.prefix { $0 != 0x3B } // hex ascii code for ;
-    input = input.dropFirst(match.count + 1)
-    return match.string
-}
-
-func parseUntilNewline(from input: inout Subbuffer) throws -> String {
-    // hex ascii code for carriage return or new line
-    let match = input.prefix { $0 != 0x0D && $0 != 0x0A }
-    input = input.dropFirst(match.count + 1)
-    return match.string
-}
-
 
 func main() async throws {
     guard CommandLine.arguments.count >= 2 else {
@@ -66,38 +65,23 @@ func main() async throws {
     }
     
     var cities = [String: City]()
-    let useSubbuffer = true
-    
-    if useSubbuffer {
-        let inputData = try Data(contentsOf: url, options: .mappedIfSafe)
-        try inputData.withUnsafeBytes { unsafeRawBufferPointer in
-            var city: String
-            var temp: String
-            
-            var subbuffer = unsafeRawBufferPointer[...]
-            while !subbuffer.isEmpty {
-                city = try parseUntilSemicolon(from: &subbuffer)
-                temp = try parseUntilNewline(from: &subbuffer)
-                var c = cities[city, default: City()]
-                let val = Double(temp)!
-                c.total += val
-                c.count += 1
-                c.min = min(val, c.min)
-                c.max = max(val, c.max)
-                cities[city] = c
-            }
-        }
-    } else {
-        for try await line in url.lines {
-            let values = line.split(separator: ";")
-            let key = String(values[0])
-            var c = cities[key, default: City()]
-            let val = Double(values[1])!
+  
+    let inputData = try Data(contentsOf: url, options: .mappedIfSafe)
+    try inputData.withUnsafeBytes { unsafeRawBufferPointer in
+        var city: String
+        var temp: String
+        var subbuffer = unsafeRawBufferPointer[...]
+        
+        while !subbuffer.isEmpty {
+            city = try subbuffer.parseUntilSemicolon()
+            temp = try subbuffer.parseUntilNewline()
+            var c = cities[city, default: City()]
+            let val = Double(temp)!
             c.total += val
             c.count += 1
             c.min = min(val, c.min)
             c.max = max(val, c.max)
-            cities[key] = c
+            cities[city] = c
         }
     }
     
